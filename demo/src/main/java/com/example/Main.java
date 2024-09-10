bf65f21aa325fb1b52e3c5acab83b01b885dca35
Juanda <yormanusme11@gmail.com>
2024-09-10 10:05:22 -0500

    guardo 1

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,43 @@
+package com.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ingrese el primer numero: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.println("Ingrese la operación a realizar (+, -, *, /, %).");
+        char operacion = scanner.next().charAt(0);
+
+        System.out.println("Ingrese el segundo numero: ");
+        double numero2 = scanner.nextDouble();
+
+        double resultado = 0;
+
+
+        if (operacion == '+') {
+            resultado = numero1 + numero2;
+        }else if (operacion == '-') {
+            resultado = numero1 - numero2;
+        }else if (operacion == '*') {
+            resultado = numero1 * numero2;
+        }else if (operacion == '/') {
+            resultado = numero1 / numero2;
+        }else if (operacion == '%') {
+            resultado = numero1 + numero2;
+        }else {
+            System.out.println("ERROR");
+        // }else if (operacion == '%') {
+
+        //     // resultado: numero1 % numero2;
+        // }else {
+        //     System.out.println("Error");
+         }
+        System.out.println("el resultado es: " + resultado);
+    
+            
+        }
+
+    }
